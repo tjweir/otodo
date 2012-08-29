6cd44ae63f957da20b3d90fc25eca8326ec0ccd4
Tyler Weir <tyler.weir@gmail.com>
2012-08-29 09:59:58 -0400

    commments

--- otodo.ml
+++ otodo.ml
@@ -1,7 +1,9 @@
 (* This is the main of otodo *)
+(* Oreilly book has a model of a db *)
+(* How do I use modules like Arg? *)
+
 type data_card = string array ;;
 type data_base = {card_index : string -> int; data : data_card list } ;;
-
 
 print_string "Hello from otodo\n";
 for i = 1 to Array.length Sys.argv - 1
